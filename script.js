128304d674636d7b33d5b5540df0a46ebd65719e
unknown <yanngsb@hotmail.com>
2023-07-10 03:58:13 -0300

    Codigo finalizado

--- script.js
+++ script.js
@@ -13,5 +13,121 @@ const productsArray = [
     price: "R$500,00",
     type: "Action Figures",
   },
-  // Produto 3 (adicione os produtos restantes abaixo)
+  // Produto 3
+  {
+    image: "./assets/img/paintings/quadro-controle.jpg",
+    name: "Controle Playstation",
+    price: "R$99,00",
+    type: "Paintings",
+  },
+  // Produto 4
+  {
+    image: "./assets/img/paintings/quadro-mario.jpg",
+    name: "Mario",
+    price: "R$275,00",
+    type: "Paintings",
+  },
+  // Produto 5
+  {
+    image: "./assets/img/paintings/quadro-pacman.jpg",
+    name: "PacMan",
+    price: "R$160,00",
+    type: "Paintings",
+  },
+  // Produto 6
+  {
+    image: "./assets/img/paintings/quadro-relogio.jpg",
+    name: "Smart Watch",
+    price: "R$120,00",
+    type: "Paintings",
+  },
+  // Produto 7
+  {
+    image: "./assets/img/actions/godzilla-action-figure.jpg",
+    name: "Godzilla",
+    price: "R$700,00",
+    type: "Action Figures",
+  },
+  // Produto 8
+  {
+    image: "./assets/img/actions/groot-action-figure.jpg",
+    name: "Groot",
+    price: "R$950,00",
+    type: "Action Figures",
+  },
+  // Produto 9
+  {
+    image: "./assets/img/actions/ironman-action-figure.jpg",
+    name: "Homem de Ferro",
+    price: "R$1030,00",
+    type: "Action Figures",
+  },
+  // Produto 10
+  {
+    image: "./assets/img/actions/starwars-action-figure.jpg",
+    name: "Sr Yoda",
+    price: "R$870,00",
+    type: "Action Figures",
+  },
+  // Produto 11
+  {
+    image: "./assets/img/actions/wolverine-action-figure.jpg",
+    name: "Wolverine",
+    price: "R$510,00",
+    type: "Action Figures",
+  },
 ];
+
+let actionFiguresArray = [];
+let paintingsArray = [];
+
+function separateItems(productsArray) {
+  for (let i = 0; i < productsArray.length; i++) {
+    const item = productsArray[i];
+    if (item.type == "Action Figures") {
+      actionFiguresArray.push(item);
+    } else if (item.type == "Paintings") {
+      paintingsArray.push(item);
+    }
+  }
+}
+
+separateItems(productsArray);
+
+function createCards(productsArray) {
+  const item = productsArray;
+  const li = document.createElement("li");
+  const image = document.createElement("img");
+  const imageContainer = document.createElement("div");
+  const name = document.createElement("h3");
+  const price = document.createElement("p");
+  const textContainer = document.createElement("div");
+
+  li.classList.add("card");
+  image.src = item.image;
+  name.classList.add("card-name");
+  name.innerText = item.name;
+  price.classList.add("card-price");
+  price.innerText = item.price;
+  textContainer.classList.add("textContainer");
+  imageContainer.classList.add("imageContainer");
+  imageContainer.append(image);
+  textContainer.append(name, price);
+  li.append(imageContainer, textContainer);
+
+  return li;
+}
+
+const actionFigureUl = document.querySelector(".actionfigure-list");
+for (let i = 0; i < actionFiguresArray.length; i++) {
+  let arr = actionFiguresArray[i];
+  let card = createCards(arr);
+  actionFigureUl.appendChild(card);
+}
+
+const paintingUl = document.querySelector(".painting-list");
+for (let i = 0; i < paintingsArray.length; i++) {
+  let arr = paintingsArray[i];
+  let card = createCards(arr);
+  paintingUl.appendChild(card);
+}
